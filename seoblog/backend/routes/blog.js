268d98ecd0b6992b63004b8404c85efa4416c9d9
oscar02r <oscary.ramirez@gmail.com>
2020-11-19 21:39:02 +0100

    Creating path to create user without database

--- seoblog/backend/routes/blog.js
+++ seoblog/backend/routes/blog.js
@@ -3,4 +3,4 @@ const { time } = require('../controllers/blog')
 const router = express.Router()
 
 router.get('/', time)
-module.exports = router 
+module.exports = router
--- seoblog/backend/server.js
+++ seoblog/backend/server.js
@@ -6,28 +6,31 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const blogRoutes = require('./routes/blog')
-
+const authRoutes = require('./routes/auth')
 const app = express()
-//DB
+// DB
 
-mongoose.connect(process.env.DATABASE_LOCAL, {useNewUrlParser:true, useCreateIndex:true, useFindAndModify:false, useUnifiedTopology:true}).then(()=>{
-    console.log('DB Connected.')
+mongoose.connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true }).then(() => {
+  console.log('DB Connected.')
 })
 
-//middlewares
+// middlewares
 app.use(morgan('dev'))
 app.use(cookieParser())
-// Cors
-if (process.env.NODE_ENV ==='development') {
-    
-    app.use(cors({origin: `${process.env.CLIENT_URL}`}))
+app.use(express.json())
+
+//Cors
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }))
 }
 
 // Routes middleware
-app.use('/api',blogRoutes)
-//port
-const port = process.env.PORT  || 9000 
+app.use('/api', blogRoutes)
+app.use('/api', authRoutes)
 
-app.listen(port, ()=>{ 
-    console.log(`Server is running on port ${port}`)
+// port
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
