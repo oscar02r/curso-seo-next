e04433e22de8b83e4c2b5dd07bc31457a3d65f3b
oscar02r <oscary.ramirez@gmail.com>
2020-11-19 15:53:13 +0100

    Adding controller blog

---
+++ seoblog/backend/routes/blog.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const { time } = require('../controllers/blog')
+const router = express.Router()
+
+router.get('/', time)
+module.exports = router 
--- seoblog/backend/server.js
+++ seoblog/backend/server.js
@@ -3,9 +3,16 @@ const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+const blogRoutes = require('./routes/blog')
 
 const app = express()
+//DB
+
+mongoose.connect(process.env.DATABASE_LOCAL, {useNewUrlParser:true, useCreateIndex:true, useFindAndModify:false, useUnifiedTopology:true}).then(()=>{
+    console.log('DB Connected.')
+})
 
 //middlewares
 app.use(morgan('dev'))
@@ -16,11 +23,8 @@ if (process.env.NODE_ENV ==='development') {
     app.use(cors({origin: `${process.env.CLIENT_URL}`}))
 }
 
-//Routes
-app.get('/api', (req, res)=>{
-        res.json({ time:Date().toString()})
-})
-
+// Routes middleware
+app.use('/api',blogRoutes)
 //port
 const port = process.env.PORT  || 9000 
 
