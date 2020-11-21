a7416f8970a8ac288abf284334f7276fd0115bb3
oscar02r <oscary.ramirez@gmail.com>
2020-11-21 13:55:07 +0100

    Tag crud ready at bakcend

---
+++ seoblog/backend/routes/tag.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const {  requireSign, adminMiddleware } = require('../controllers/auth')
+const { create, list, read, remove} = require('../controllers/tag')
+
+
+const { runValidation } = require('../validators')
+const {tagCreateValidator } = require('../validators/tag')
+
+router.post('/tag', tagCreateValidator, runValidation, requireSign, adminMiddleware, create )
+router.get('/tags', list)
+router.get('/tag/:slug', read)
+router.delete('/tag/:slug',requireSign, adminMiddleware, remove)
+
+
+module.exports= router  
