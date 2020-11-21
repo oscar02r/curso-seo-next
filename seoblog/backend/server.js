e01aeaf0e39abccd72ab4c73e5e50b0c942d6a14
oscar02r <oscary.ramirez@gmail.com>
2020-11-21 12:07:28 +0100

    Listed, read and delete category

--- seoblog/backend/server.js
+++ seoblog/backend/server.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 const blogRoutes = require('./routes/blog')
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
 
 const app = express()
 // DB
@@ -29,6 +30,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api', blogRoutes)
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
+app.use('/api', categoryRoutes)
 
 // port
 const port = process.env.PORT || 9000
