cd2e9216480bef9662ec6c8ed1ad758eef295bdd
oscar02r <oscary.ramirez@gmail.com>
2020-11-26 23:45:26 +0100

    Creating actions update delete and list blogs

--- seoblog/frontend/actions/blog.js
+++ seoblog/frontend/actions/blog.js
@@ -61,3 +61,45 @@ export const listRelated = (blog) =>{
     })
     .catch(error => console.log(error))
 }
+
+export const list = () =>{
+    return fetch(`${API}/api/blogs`,{
+        method:'GET'
+    }).then( response =>{
+        return response.json()
+    })
+    .catch(console.log)
+}
+
+export const removeBlog = (slug, token) =>{
+    
+    return fetch(`${API}/api/blog/${slug}`,{
+        method:'DELETE',
+        headers:{
+            Accept:'application/json',
+             'Content-Type':'application/json',
+            'Authorization':`Bearer ${token}`,
+        },
+    
+    })
+    .then( response => {  
+        return response.json()
+    })
+    .catch(error => console.log(error))
+}
+
+export const updateBlog = (blog, token, slug) =>{
+    
+    return fetch(`${API}/api/blog/${slug}`,{
+        method:'PUT',
+        headers:{
+            Accept:'application/json',
+            'Authorization':`Bearer ${token}`,
+        },
+        body:blog
+    })
+    .then( response => {  
+        return response.json()
+    })
+    .catch(error => console.log(error))
+}
