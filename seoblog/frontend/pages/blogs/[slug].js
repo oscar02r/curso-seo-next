4ecf1e5a720027e2e8379be8a30b030e6f7923bb
oscar02r <oscary.ramirez@gmail.com>
2020-11-26 20:28:12 +0100

    Config of seo of sigle blogs

--- seoblog/frontend/pages/blogs/[slug].js
+++ seoblog/frontend/pages/blogs/[slug].js
@@ -6,7 +6,37 @@ import moment from "moment";
 import reactHtmlParser from "react-html-parser";
 import { APP_NAME, DOMAIN, API, FB_APP_ID } from "../../config";
 
-const SingleBlog = ({ blog }) => {
+const SingleBlog = ({ blog, query }) => {
+  const head = () => {
+    return (
+      <Head>
+        <title>blog.title | {APP_NAME}</title>
+        <meta
+          name="description"
+          content={blog.mdesc}
+        />
+        <link rel="canonical" href={`${DOMAIN}/blogs/${query.slug}`} />
+        <meta
+          property="og:title"
+          content={`${blog.title} | ${APP_NAME}`}
+        />
+        <meta
+          property="og:description"
+          content={blog.mdesc}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`${DOMAIN}/blogs/${query.slug}`} />
+        <meta property="og:site_name" content={APP_NAME} />
+        <meta property="og:image" content={`${API}/api/blog/photo/${blog.slug}`}/>
+        <meta
+          property="og:image:secure_url"
+          content={`${API}/api/blog/photo/${blog.slug}`}
+        />
+        <meta property="og:image:type" content="image/jpg" />
+        <meta property="og:fb_app_id" content={FB_APP_ID} />
+      </Head>
+    );
+  };
   const showBlogCategories = (blog) => {
     return blog.categories.map((c, i) => (
       <Link key={i} href={`/categories/${c.slug}`}>
@@ -22,6 +52,8 @@ const SingleBlog = ({ blog }) => {
     ));
   };
   return (
+    <>
+    {head()}
     <main>
       <article>
         <div className="container-fluid">
@@ -35,15 +67,20 @@ const SingleBlog = ({ blog }) => {
             </div>
           </section>
           <section>
-            <p className="lead mt-3 mark">
-              Written by {blog.postedBy.name} | Published{" "}
-              {moment(blog.updatedAt).fromNow()}
-            </p>
-            <div className="pb-3">
-              {showBlogCategories(blog)}
-              {showBlogTag(blog)}
-              <br />
-              <br />
+            <div className="container">
+              <h1 className="display-2 pb-3 pt-3 text-center font-weight-bold">
+                {blog.title}
+              </h1>
+              <p className="lead mt-3 mark">
+                Written by {blog.postedBy.name} | Published{" "}
+                {moment(blog.updatedAt).fromNow()}
+              </p>
+              <div className="pb-3">
+                {showBlogCategories(blog)}
+                {showBlogTag(blog)}
+                <br />
+                <br />
+              </div>
             </div>
           </section>
         </div>
@@ -62,16 +99,17 @@ const SingleBlog = ({ blog }) => {
         </div>
       </article>
     </main>
+    </>
   );
 };
 
 SingleBlog.getInitialProps = ({ query }) => {
   return singleBlog(query.slug).then((data) => {
-    console.log("Initial props", data);
+  
     if (data.error) {
       console.log(data.error);
     } else {
-      return { blog: data };
+      return { blog: data, query };
     }
   });
 };
